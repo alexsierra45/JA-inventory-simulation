d0f3a37db9e2ea96de98f8a2150e85ab23b85e1b
Alex <alexsierraalcala@gmail.com>
2024-02-13 11:03:29 -0500

    first commit

---
+++ inventory_simulation.py
@@ -0,0 +1,15 @@
+class InventorySimulation:
+    def __init__(self):
+        pass
+
+    def demand(self):
+        pass
+
+    def receive_order(self):
+        pass
+
+    def place_order(self):
+        pass
+
+    def simulate(self):
+        pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from inventory_simulation import InventorySimulation
+
+def main():
+    simulation = InventorySimulation()
+    simulation.simulate()
